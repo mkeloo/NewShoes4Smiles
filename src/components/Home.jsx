93da71d49174bb2f6627b350b707e1deb510f2bc
mkeloo <mkeloo@ufl.edu>
2023-02-27 03:14:55 -0500

    added stats page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 // import Hero from '../pages/Hero';
 import Hero2 from '../pages/Hero2';
-import About from '../pages/About/About';
+// import About from '../pages/About/About';
 import Events from '../pages/Events/Events';
+import Stats from '../pages/Stats';
 
 const Home = () => {
   return (
     <section>
       <Hero2 />
-      <About />
+      <Stats />
       <Events />
     </section>
   );
---
+++ src/pages/Stats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Stats = () => {
+  return (
+    <section className="w-[100vw] h-[100vh] bg-[#89CFF0] mb-8 md:mb-0 mt-4  py-5 flex justify-center items-center -z-10 ">
+      <h1 className="lg:text-6xl md:text-4xl text-[#060229]  text-3xl font-bold py-6 pb-4">
+        Stats
+      </h1>
+    </section>
+  );
+};
+
+export default Stats;
